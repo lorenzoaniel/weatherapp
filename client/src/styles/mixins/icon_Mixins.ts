58ed7744a66ee493ec874d0ada738fd342205f23
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-29 20:29:56 -0600

    Update icon_Mixins.ts

--- client/src/styles/mixins/icon_Mixins.ts
+++ client/src/styles/mixins/icon_Mixins.ts
@@ -1,17 +1,12 @@
-import {
-	DefaultTheme,
-	FlattenSimpleInterpolation,
-	ThemedCssFunction,
-	css,
-} from "styled-components";
+import { FlattenSimpleInterpolation, css } from "styled-components";
 
-export interface icon_MixinsType {
-	navButtonStyle: FlattenSimpleInterpolation;
-}
+// export interface icon_MixinsType {
+// 	navButtonStyle: FlattenSimpleInterpolation;
+// }
 
-export const icon_Mixins: icon_MixinsType = {
-	navButtonStyle: css`
-		height: 100%;
-		width: 100%;
-	`,
+export const icon_Mixins: any = {
+	navButtonStyle: {
+		height: "100%",
+		width: "100%",
+	},
 };
