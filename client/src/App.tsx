f17aa76ad17181642a19bc16ec303f9b65a88c22
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-29 20:27:05 -0600

    App: theme provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
 import React, { Suspense } from "react";
-import { AnimatePresence } from "framer-motion";
-import { ThemeProvider } from "styled-components";
+import { AnimatePresence, motion } from "framer-motion";
+import styled, { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { Routes, Route, Outlet } from "react-router";
 import Loading from "./pages/loading/Loading";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/home/Home";
-import { default_Theme, default_ThemeType } from "./styles/themes/default_Theme";
+import { default_Theme } from "./styles/themes/default_Theme";
 
 const App: React.FC = () => {
 	return (
-		<ThemeProvider theme={default_Theme as default_ThemeType}>
+		<ThemeProvider theme={default_Theme}>
 			<GlobalStyle />
 
 			<Suspense fallback={<Loading />}>
